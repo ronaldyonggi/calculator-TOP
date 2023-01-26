799ac5358dc06deef38bd574d9f46c36f80cdf53
Ronald Yonggi <ronald.yonggi@ukg.com>
2023-01-26 18:10:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const {add, subtract, multiply, divide, operate} = require('./operations/operations')
+
+
+// A variable that stores current value. Display on screen
+
+// operator symbols are also displayed on screen
+
+
